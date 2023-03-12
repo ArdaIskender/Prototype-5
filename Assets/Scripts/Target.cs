667f40a0ada0c974aa04ff2decc5e9b4a0924a33
Arda Iskender <iskenderarda@gmail.com>
2023-03-12 16:48:55 +0300

    5.2.5.1

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private GameManager gameManager;
 
     private float minSpeed = 12;
     private float maxSpeed = 16;
@@ -16,6 +17,7 @@ public class Target : MonoBehaviour
     void Start()
     {
         targetRb= GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
 
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
 
@@ -32,6 +34,7 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        gameManager.UpdateScore(5);
     }
 
     private void OnTriggerEnter(Collider other)
